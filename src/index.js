6e0547cb2020cb1af990fda74ea42242f6ee46f9
Yoon Wai Yan <waiyanyoon@gmail.com>
2018-03-17 16:00:56 +0800

    Add height styles for React Data Grid

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import 'antd/dist/antd.css';
 import App from './App';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
